2e6ebe5f6856134b2212058ff1a0a115c7f8de7c
vominhtuan1 <19522487@gm.uit.edu.vn>
2022-04-17 10:11:32 +0700

    revenue backend continue

--- src/main/java/app/clothing_management/repository/OrderRepository.java
+++ src/main/java/app/clothing_management/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package app.clothing_management.repository;
 import app.clothing_management.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends MongoRepository<Order, String> {
+    List<Order> getOrderByDateOrderBetween(Date from, Date to);
 }
